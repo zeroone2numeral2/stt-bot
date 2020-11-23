0b4f9f510c026d5b2a9c7ac2ace21a8a1f730633
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-11-23 19:25:51 +0100

    Fixed opus sample rate decoding

--- google/speechtotext/stt.py
+++ google/speechtotext/stt.py
@@ -1,6 +1,7 @@
 import io
 import os
 import logging
+import struct
 from typing import List
 
 # noinspection PyPackageRequirements
@@ -16,6 +17,7 @@ from google.cloud.speech import (
 )
 # noinspection PyPackageRequirements
 from telegram import Message
+# from tinytag import TinyTag
 
 from google.clients import speech_client
 from google.clients import storage_client
@@ -40,7 +42,8 @@ class VoiceMessage:
         self.file_path = os.path.join(download_dir, self.file_name)
         self.duration = duration
         self.short = True
-        self.hertz_rate = self.OPUS_HERTZ_RATE[0]
+        self.hertz_rate = None  # self.OPUS_HERTZ_RATE[0]
+        self.channels = None  # currently not used
         self.client: SpeechClient = speech_client
         self.max_alternatives = max_alternatives
         self.recognition_audio: [RecognitionAudio, None] = None
@@ -97,8 +100,68 @@ class VoiceMessage:
 
         return response
 
+    def _read_sample_rate(self):
+        # for this to work, we need this line https://github.com/devsnd/tinytag/blob/9c7bd666f753d0eb6eb8573c5574510ed366641e/tinytag/tinytag.py#L802
+        # to be set to "sr" (and not to a costant)
+        # file_metadata = TinyTag.get(self.file_path)
+        # self.hertz_rate = file_metadata.samplerate
+        raise NotImplementedError
+
+    @staticmethod
+    def _parse_pages(fh):
+        # for the spec, see: https://wiki.xiph.org/Ogg
+        previous_page = b''  # contains data from previous (continuing) pages
+        header_data = fh.read(27)  # read ogg page header
+        while len(header_data) != 0:
+            header = struct.unpack('<4sBBqIIiB', header_data)
+            # https://xiph.org/ogg/doc/framing.html
+            oggs, version, flags, pos, serial, pageseq, crc, segments = header
+            # print(oggs, version, flags, pos, serial, pageseq, crc, segments)
+            # self._max_samplenum = max(self._max_samplenum, pos)
+
+            if oggs != b'OggS' or version != 0:
+                raise ValueError('not a valid ogg file')
+
+            segsizes = struct.unpack('B' * segments, fh.read(segments))
+            total = 0
+
+            for segsize in segsizes:  # read all segments
+                total += segsize
+                if total < 255:  # less than 255 bytes means end of page
+                    yield previous_page + fh.read(total)
+                    previous_page = b''
+                    total = 0
+
+            if total != 0:
+                if total % 255 == 0:
+                    previous_page += fh.read(total)
+                else:
+                    yield previous_page + fh.read(total)
+                    previous_page = b''
+
+    def _parse_sample_rate(self):
+        # all opus decoders will return 48kHz when decoding opus files, even if the header contains a diiferent value
+        # google wants the header value to be passed, or the request will fail
+        # see https://stackoverflow.com/q/39186282
+        # courtesy of TinyTag: https://github.com/devsnd/tinytag
+
+        with io.open(self.file_path, "rb") as fh:
+            for packet in self._parse_pages(fh):
+                walker = io.BytesIO(packet)
+                if packet[0:8] == b'OpusHead':  # parse opus header (MUST be opus header)
+                    # https://www.videolan.org/developers/vlc/modules/codec/opus_header.c
+                    # https://mf4.xiph.org/jenkins/view/opus/job/opusfile-unix/ws/doc/html/structOpusHead.html
+                    walker.seek(8, os.SEEK_CUR)  # jump over header name
+                    (version, ch, _, sample_rate, _, _) = struct.unpack("<BBHIHB", walker.read(11))
+                    if (version & 0xF0) == 0:  # only major version 0 supported
+                        self.channels = ch
+                        self.hertz_rate = sample_rate  # internally opus always uses 48khz
+                        break  # read the header just as much as we need
+
     def recognize(self, max_alternatives: [int, None] = None, *args, **kwargs) -> [List[SpeechRecognitionAlternative], None]:
         self._generate_recognition_audio()
+        self._parse_sample_rate()
+        logger.debug("file sample rate (hertz rate): %d", self.hertz_rate)
 
         # noinspection PyTypeChecker
         self.recognition_config = RecognitionConfig(
