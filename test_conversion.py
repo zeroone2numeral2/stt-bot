854507bed0b74578926a8bfa6a755afdb999d367
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-11-23 16:02:32 +0100

    Added note on opus hertz rate

--- test_conversion.py
+++ test_conversion.py
@@ -1,3 +1,4 @@
 from pydub import AudioSegment
-audio_segment = AudioSegment.from_ogg('downloads/23646077_12619.ogg')
+open('downloads/23646077_12619.ogg')
+audio_segment = AudioSegment.from_ogg('./downloads/23646077_12619.ogg')
 audio_segment.export("testme.flac", format="flac")
