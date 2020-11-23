3d188fb9e7ddc80dec7347b60de2df7124d1dfe7
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-11-24 00:33:29 +0100

    db models

--- bot/database/models/user.py
+++ bot/database/models/user.py
@@ -7,7 +7,9 @@ class User(Base):
     __tablename__ = 'users'
 
     user_id = Column(Integer, primary_key=True)
+    enabled = Column(Boolean, default=True)
     tos_accepted = Column(Boolean, default=False)
+    whitelisted = Column(Boolean, default=False)
 
     def __init__(self, user_id):
         self.user_id = user_id
