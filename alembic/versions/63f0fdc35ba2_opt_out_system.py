ca6d5a45bfe77584f22a7f5b37bf4519ffbd6889
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-03-26 16:41:56 +0100

    Added opt-out alembic migration

---
+++ alembic/versions/63f0fdc35ba2_opt_out_system.py
@@ -0,0 +1,24 @@
+"""opt_out_system
+
+Revision ID: 63f0fdc35ba2
+Revises: 57f90fdab128
+Create Date: 2021-03-26 16:36:50.994494
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63f0fdc35ba2'
+down_revision = '57f90fdab128'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(table_name="users", column_name="tos_accepted", new_column_name="opted_out")
+
+
+def downgrade():
+    pass
