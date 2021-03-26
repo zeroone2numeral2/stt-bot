aa1988d3c41289bd38d44068326476afc6449c37
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-03-26 18:10:50 +0100

    Updated /start text

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -23,7 +23,8 @@ logger = logging.getLogger(__name__)
 TEXT = """Ciao! Sono un bot che permette di trascrivere i messaggi vocali inviati dagli membri dei gruppi in cui vengo \
 aggiunto, utilizzando la <a href="https://cloud.google.com/speech-to-text/">trascrizione vocale di Google</a>
 
-Usa /tips o /help per ricevere alcuni suggerimenti sull'utilizzo del bot"""
+Usa /tips o /help per ricevere alcuni suggerimenti sull'utilizzo del bot. Usa /disclaimer per saperne di più su come \
+vengono gestiti i tuoi dati. Usa /optout se vuoi che il bot non trascriva i vocali che invii nei gruppi"""
 
 
 @decorators.catchexceptions()
