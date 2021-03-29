47fca842aa9e048280b30812be2c27b02719223a
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-03-29 18:10:18 +0200

    MessageToDelete: removed can_delete property
    Two possible paths:
    - check whether the bot is admin before queueing a message for deletion
    - check whether the bot is an admin when deleting a message
    - always send the request and delete the record if telegram raises an axception

--- bot/database/models/message_to_delete.py
+++ bot/database/models/message_to_delete.py
@@ -11,15 +11,16 @@ class MessageToDelete(Base):
 
     chat_id = Column(Integer, ForeignKey('chats.chat_id', ondelete="CASCADE"), primary_key=True)
     message_id = Column(Integer, primary_key=True)
+    sender_is_self = Column(Boolean, nullable=False)  # we need to know if the message was sent by us
     sent_on = Column(DateTime, default=datetime.datetime.utcnow)
     delete_after = Column(Integer, nullable=True)  # in seconds
-    can_delete = Column(Boolean, nullable=True)  # in case the bot doesn't have the permission
 
     chat = relationship("Chat", back_populates="messages_to_delete")
 
-    def __init__(self, chat_id, message_id, delete_after: int = None):
+    def __init__(self, chat_id, message_id, sender_is_self, delete_after: int = None):
         self.chat_id = chat_id
         self.message_id = message_id
+        self.sender_is_self = sender_is_self
         self.delete_after = delete_after
 
 
